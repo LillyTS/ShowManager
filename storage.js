8384b80697f23c4c8c54e3435cf80c83537e6e94
Liliana Santos <75341426+LillyTS@users.noreply.github.com>
2022-05-21 18:04:07 +0100

    Delete Show
    - Added icons to delete and edit shows
    - Added the code to delete icon
    - Edit doesn't work yet

--- storage.js
+++ storage.js
@@ -1,70 +1,128 @@
-var shows = [];
+let shows = [];
+let lastid = -1;
 
-var ul = document.getElementById("showslist");
+let ul = document.getElementById("showslist");
 
-var confirmbutton = document.getElementById("confirm");
+let confirmbutton = document.getElementById("confirm");
 confirmbutton.addEventListener("click", function () { addNewShowStorage(); });
-// var searchbutton = document.getElementById('searchbutton');
+// let searchbutton = document.getElementById('searchbutton');
 // searchbutton.addEventListener("click", function () { search(); });
-var input = document.getElementById('search');
+let input = document.getElementById('search');
 input.addEventListener("input", function () { search(); });
 
-chrome.storage.sync.get(['shows'], function (result) {
+chrome.storage.sync.get(['lastid','shows'], function (result) {
   shows = result.shows;
-  for (var i = 0; i < shows.length; i++) {
-    var show = shows[i];
-    var li = document.createElement('li');
-    li.appendChild(document.createTextNode(show.Name + " | S" + show.Season + "E" + show.Episode));
-    ul.appendChild(li);
+  lastid = result.lastid ?? -1;
+  if (shows != undefined && shows.length > 0) {
+    for (let i = 0; i < shows.length; i++) {
+
+      let show = shows[i];
+      let li = document.createElement('li');
+      li.setAttribute("id",show.id);
+
+      let editButton = document.createElement('input');
+      editButton.type = "image";
+      editButton.src = "pen-to-square-solid.svg";
+      editButton.style = "width: 17px; height: 17px;";
+      editButton.onclick = () => editShow(li); 
+
+      let deleteButton = document.createElement('input');
+      deleteButton.type = "image";
+      deleteButton.src = "trash-can-solid.svg";
+      deleteButton.style = "width: 17px; height: 17px;";
+      deleteButton.onclick = () => deleteShow(li); 
+
+      li.appendChild(editButton);
+      li.appendChild(deleteButton);
+      li.appendChild(document.createTextNode(show.Name + " | S" + show.Season + "E" + show.Episode));
+      ul.appendChild(li);
+    }
   }
 });
 
 
 function addNewShowStorage()
 {
-  var showname = document.getElementById("showname").value;
-  var seasonnumber = document.getElementById("seasonnumber").value;
-  var episodenumber = document.getElementById("episodenumber").value;
-  var newShow = {
+  let showname = document.getElementById("showname").value;
+  let seasonnumber = document.getElementById("seasonnumber").value;
+  let episodenumber = document.getElementById("episodenumber").value;
+  let id = lastid + 1; 
+  let newShow = {
+    "id": id,
     "Name": showname,
     "Season": seasonnumber,
     "Episode": episodenumber
   };
   if (showname != "")
   {
+    if (shows == undefined) {
+      shows = [];
+    }
     shows.push(newShow);
   }
-  
-  var li = document.createElement('li');
+
+  let li = document.createElement('li');
+
+  li.setAttribute("id", id);
+
+  let editButton = document.createElement('input');
+  editButton.setAttribute("type", "image");
+  editButton.src = "pen-to-square-solid.svg";
+  editButton.style = "width: 17px; height: 17px;";
+  editButton.onclick = () => editShow(li); 
+
+
+  let deleteButton = document.createElement('input');
+  deleteButton.setAttribute("type", "image");
+  deleteButton.src = "trash-can-solid.svg";
+  deleteButton.style = "width: 17px; height: 17px;"; 
+  deleteButton.onclick = () => deleteShow(li); 
+
+
+  li.appendChild(editButton);
+  li.appendChild(deleteButton);
   li.appendChild(document.createTextNode(newShow.Name + " |   S" + newShow.Season + "E" + newShow.Episode));
   ul.appendChild(li);
-  chrome.storage.sync.set({ "shows": shows });
+  chrome.storage.sync.set({ "lastid": newShow.id,  "shows": shows });
+
+
   // console.log(shows);
 }
 
-function editShow(showname,seasonnumber,episodenumber)
+function deleteShow(showElement)
+{
+  let index = shows.findIndex(function (shows) {
+    return shows.id === showElement.id;    
+  });
+  console.log( index, "  ", showElement.id,  "   ", shows.id);
+
+  shows.splice(index);
+  showElement.remove();
+  chrome.storage.sync.set({ "shows": shows });
+}
+
+function editShow(showElement)
 {
   var index = shows.findIndex(function (shows) {
-    return shows.Name === showname;
+    return shows.id === showElement.id;
   });
-
-  if (index == -1)
-  {
-    alert('The show you\'re trying to edit does not exist');
-  }
-  else {
-    shows[index].Season = seasonnumber;
-    shows[index].Episode = episodenumber;
-    chrome.storage.sync.set({ "shows": shows });
-  }
+    console.log( index, "  ", showElement.id);
+    let season = prompt("Season", "Season");
+    let episode = prompt("Episode", "Episode");
+    console.log(season, episode);
+    if(season != null && episode != null && shows.id === index) {
+      shows.Season = season;
+      shows.Episode = episode;
+      chrome.storage.sync.set({ "shows": shows });
+    }
 }
 
 function search() {
-  var filter = input.value.toUpperCase();
-  var li = document.getElementsByTagName("li");
+  let filter = input.value.toUpperCase();
+  let li = document.getElementsByTagName("li");
   // Loop through all list items, and hide those that don't match the search query
   for (i = 0; i < li.length; i++) {
-    var txtValue = li[i].innerHTML;
+    let txtValue = li[i].innerHTML;
     if (txtValue.toUpperCase().indexOf(filter) > -1) {
       li[i].style.display = "";
     } else {
